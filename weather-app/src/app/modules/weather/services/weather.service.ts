3fdf4f330106b34b63f9ba3fdc4b87fd4109347d
Luiz Glomyer <glomyerjunior@hotmail.com>
2023-09-09 15:30:06 -0400

    chore: add environment variables

--- weather-app/src/app/modules/weather/services/weather.service.ts
+++ weather-app/src/app/modules/weather/services/weather.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/app/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private API_KEY = ""
+  private apiKey = environment.apiKey
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +17,7 @@ export class WeatherService {
         q: cityName,
         units: "metric",
         mode: "json",
-        appid: this.API_KEY
+        appid: this.apiKey
       }
     })
   }
